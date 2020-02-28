1f4a7e2f4b056954bd5d470dcfc16db907ccfece
Stavros Filosidis <stavrosfil@gmail.com>
2020-02-28 16:24:04 +0000

    Improve sync script and move antigen to dotfiles

--- .zshrc
+++ .zshrc
@@ -4,7 +4,7 @@ export PATH=$HOME/bin:/usr/local/bin:$PATH
 # Path to your oh-my-zsh installation.
 # export ZSH="$HOME/.oh-my-zsh"
 
-source $HOME/antigen.zsh
+source $HOME/dotfiles/antigen.zsh
 
 # Export sync script thingy
 export already_setup=true
--- sync.sh
+++ sync.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-cd $HOME
-
-# Install all snap packages
-snap-packages.txt xargs sudo snap install
-
 # Install packages from a file, and remove all the comments inside!
 sudo apt install $(grep -vE "^\s*#" apt-packages.txt | tr "\n" " ")
 
@@ -18,30 +13,31 @@ DOTS="$HOME/dotfiles"
 chsh -s $(which zsh)
 
 # Initial setup of folders, repo and symlinks!
-if [ "$already_setup" != true ]; then
-	echo "Setting up repos directories"
+#if [ "$already_setup" != true ]; then
 
-	cd $HOME
-	rm -rf .scripts .zshrc .vimrc
-	cd .config
-	rm -rf i3 i3blocks rofi .vim zathura compton kitty ranger
-	sudo rm -rf /etc/X11/xorg.conf /etc/X11/xorg.conf.d
+echo "Setting up repos directories"
 
-	cd $HOME
-	mkdir -p repos
+cd $HOME
+rm -rf .scripts .zshrc .vimrc
+cd .config
+rm -rf .vim ranger
 
-	cd repos
+if [ -d "$DOTS" ]; then
+	cd $DOTS
+	git pull origin master
+else
 	echo "Cloning repo..."
 	git clone https://github.com/stavrosfil/dotfiles
-
-	# Symbolic links!
-	ln -sv $DOTS/ranger $HOME/.config
-	ln -sv $DOTS/vim/.vim $HOME/.config/
-	ln -sv $DOTS/vim/.vimrc $HOME/
-	ln -sv $DOTS/.zshrc $HOME/
 fi
 
-cd $DOTS
-git pull
+
+# Symbolic links!
+ln -sv $DOTS/ranger $HOME/.config
+ln -sv $DOTS/vim/.vim $HOME/.config/
+ln -sv $DOTS/vim/.vimrc $HOME/
+ln -sv $DOTS/.zshrc $HOME/
+
+#fi
+
 
 echo $PWD
